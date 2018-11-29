a0e04fcb189f48f418d576c2919a426fa89e0a52
sonnysong <sonnysong01@hotmail.com>
2018-11-28 21:43:29 -0700

    update

--- src/codes/Main.java
+++ src/codes/Main.java
@@ -11,8 +11,11 @@ public class Main {
 		list.add(3);
 		list.add(4);
 		list.add(5);
-		//list.add(6);
-		//list.add(7);
+		list.add(6);
+		list.add(7);
+		list.add(8);
+		list.add(9);
+		list.add(10);
 		
 		list.swap(2,3);
 		
